799dd8b76ba0adb8efbad68d475b0fc0a667e732
FrancisBaileyH <seveneleven987@yahoo.com>
2013-07-18 17:26:43 -0600

    Addition of a simple 404 page

---
+++ Application/Views/404.php
@@ -0,0 +1,14 @@
+		<section id="fileform">
+			<div class="inner">
+				<div class="non-semantic-protector">
+				<h1 class="ribbon">
+					<strong class="ribbon-content">~ Uploadr ~</strong>
+				</h1>
+				</div>
+				<div class="innerchild">
+					<div class="404">
+						<h1>404. Looks like this page doesn't exist!</h1>
+					</div>
+				</div>
+			</div>
+		</section>
