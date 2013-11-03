1a059ff2d2ccff777c110ad959bfa9efa3c3bedf
katten <katten@kattenmedia.com>
2013-11-03 12:15:29 -0800

    Fixed Parent Directory Navigation Issue

--- Application/Config/config.php
+++ Application/Config/config.php
@@ -29,13 +29,6 @@ $config['max_file_size'] = 209715200;//ini_get('upload_max_file_size');
 
 
 
-/*
- *   Maximum size allowed
-*/
-$config['max_post_size'] =  ini_get('upload_max_post_size') - 1;
-
-
-
  
 /*
  * Maximum files to be uploaded @ a time
